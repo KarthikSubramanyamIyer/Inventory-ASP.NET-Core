8b810bc23188cd4f0989c432124f52e5c7c2580b
unknown <karthik.s.hyd@gmail.com>
2022-02-27 19:24:00 +0530

    First Commit

---
+++ InventoryBegininers/Controllers/UnitController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryBegininers.Data;
+using Microsoft.EntityFrameworkCore;
+using InventoryBegininers.Models;
+using InventoryBegininers.Interfaces;
+
+namespace InventoryBegininers.Controllers
+{
+    public class UnitController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Unit> units = _UnitRepo.GetItem(); 
+            return View(units);
+        }
+        private readonly InventoryContext _context;
+
+        private readonly IUnit _UnitRepo;
+
+        public UnitController(InventoryContext context,IUnit unitrepo)
+        {
+            _context = context;
+            _UnitRepo = unitrepo;
+        }
+        public IActionResult Create()
+        {
+            Unit unit = new Unit();
+            return View(unit);
+        }
+        [HttpPost]
+        public IActionResult Create(Unit unit)
+        {
+            try
+            {
+               unit= _UnitRepo.Create(unit);
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            Unit unit = _UnitRepo.GetUnit(id);
+            return View(unit);
+        }
+        public IActionResult Edit(int id)
+        {
+            Unit unit = _UnitRepo.GetUnit(id);
+            return View(unit);
+        }       
+        [HttpPost]
+        public IActionResult Edit(Unit unit)
+        {
+            try
+            {
+                unit = _UnitRepo.Edit(unit);
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int id)
+        {
+            Unit unit = _UnitRepo.GetUnit(id);
+            return View(unit);
+        }
+        [HttpPost]
+        public IActionResult Delete(Unit unit)
+        {
+            try
+            {
+                unit = _UnitRepo.Delete(unit);
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction(nameof(Index));
+        }       
+    }
+}
---
+++ InventoryBegininers/Data/InventoryContext.cs
@@ -0,0 +1,19 @@
+﻿using InventoryBegininers.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryBegininers.Data
+{
+    public class InventoryContext:DbContext
+    {
+        public InventoryContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public virtual DbSet<Unit> Units { get; set; }
+    }
+}
